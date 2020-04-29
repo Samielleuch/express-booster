97fbdf828ddc11733d04c7229c00804ea0799090
Samielleuch <Sami.Elleuch@insat.u-carthage.tn>
2020-04-30 00:27:54 +0100

    Added CLI interactive menu and package.json Generation

--- bin/RestGenCLI.js
+++ bin/RestGenCLI.js
@@ -1,27 +1,53 @@
 #!/usr/bin/env node
 
-const ora = require("ora");
 const fs = require("fs");
 let basedir = "./src/";
 let controlDir = "controllers";
 let routerDir = "routers";
 let configDir = "config";
 
+const cli = require("clui");
+const shell = require("shelljs");
+const Spinner = cli.Spinner;
+const clear = require("clear");
+const spawn = require("child_process").spawn;
+const chalk = require("chalk");
+const figlet = require("figlet");
+
+//creating npm package
+
 // Creating the Folders
-let throbber = ora("Creating Folders").start();
-fs.mkdirSync(basedir);
-fs.mkdirSync(basedir + controlDir);
-fs.mkdirSync(basedir + routerDir);
-fs.mkdirSync(basedir + configDir);
-throbber.stop();
+// fs.mkdirSync(basedir);
+// fs.mkdirSync(basedir + controlDir);
+// fs.mkdirSync(basedir + routerDir);
+// fs.mkdirSync(basedir + configDir);
 
 // Copying Files !
-const FileMaker = require("../lib/FileMaker");
-
+const packjson = require("../lib/packjsonMaker");
+const Cliquery = require("../lib/CLIquery")
 //Copying server.js
-FileMaker.copyFile(basedir + "server.js", basedir + "server.js", (err) => {
-  console.log(err);
-});
+// FileMaker.copyFile(basedir + "server.js", basedir + "server.js", (err) => {
+//   console.log(err);
+// });
+
+const init = async () => {
+  clear();
+  console.log(
+    chalk.red(
+      figlet.textSync("Expressify :D ", {
+        horizontalLayout: "full",
+      })
+    )
+  );
+};
 
 
-console.log("this is my cli app ");
+
+(async () => {
+  init();
+  const answer = await Cliquery.ask();
+ // console.log(answer);
+   await packjson.createPackageJson(shell , chalk , answer);
+   await packjson.addDep(chalk , Spinner , answer );
+  //await createPackageJson(JSON.stringify(packageJson, null, 1));
+})();
