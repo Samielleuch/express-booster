2aeb60abeee3a4429c2c95aa9ba7c3fc88dbc3ff
Sami <sami.elleuch@insat.u-carthage.tn>
2020-07-03 18:36:07 +0100

    added Template Renderer and dynamic rendering according to user input

--- bin/RestGenCLI.js
+++ bin/RestGenCLI.js
@@ -1,34 +1,15 @@
 #!/usr/bin/env node
-
 const fs = require("fs");
-
 const Listr = require("listr");
 const cli = require("clui");
-const shell = require("shelljs");
 const Spinner = cli.Spinner;
 const clear = require("clear");
 const spawn = require("child_process").spawn;
 const chalk = require("chalk");
 const figlet = require("figlet");
-
-//creating npm package
-
-// Creating the Folders
-// fs.mkdirSync(basedir);
-// fs.mkdirSync(basedir + controlDir);
-// fs.mkdirSync(basedir + routerDir);
-// fs.mkdirSync(basedir + configDir);
-
-// Copying Files !
-const tests = require("../lib/ServerMaker");
+const FileMaker = require("../lib/ServerMaker");
 const Cliquery = require("../lib/CLIquery");
 const controller = require("../lib/Controller");
-
-//Copying server.js
-// FileMaker.copyFile(basedir + "server.js", basedir + "server.js", (err) => {
-//   console.log(err);
-// });
-
 const init = async () => {
   clear();
   console.log(
@@ -42,16 +23,8 @@ const init = async () => {
 
 (async () => {
   init();
-  //await tasks.run();
-
   const answer = await Cliquery.ask();
-  //console.log(answer);
   controller.init(answer);
-  tests.init(answer);
-
-  //await controller.tasks.run()
-  tests.CreateServerFile();
-  // await packjson.createPackageJson(shell , chalk , answer);
-  //await packjson.addDep(chalk , Spinner , answer );
-  //await createPackageJson(JSON.stringify(packageJson, null, 1));
+  FileMaker.init(answer);
+  await controller.tasks.run();
 })();
